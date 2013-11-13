21ee2ea26c434eb5790e28ec636445f9f10631a4
zsjforcn <zhangsanji@gmail.com>
2013-11-13 12:18:18 +0800

    index

--- www/sony_o2o/__init__.py
+++ www/sony_o2o/__init__.py
@@ -33,6 +33,7 @@ _api.add_resource(v1_guest, '/v1/guest', '/v1/guest/<int:id>')
 
 from sony_o2o.views.guest.Guest import GuestList as v1_guest_list
 _api.add_resource(v1_guest_list, '/v1/guests')
+_api.add_resource(v1_guest_list, '/v1/guest/list')
 
 from sony_o2o.views.product.Product import Product as v1_product
 _api.add_resource(v1_product, '/v1/product/<int:id>')
@@ -54,9 +55,6 @@ _api.add_resource(v1_manual, '/v1/manual', '/v1/manual/<int:id>')
 
 from sony_o2o.views.product.Product import Manuals as v1_manuals
 _api.add_resource(v1_manuals, '/v1/manuals')
-
-
-_api.add_resource(v1_guest_list, '/v1/guest/list')
 
 from sony_o2o.views import index
 app.register_blueprint(index.BP, url_prefix='/')
--- www/sony_o2o/views/index.py
+++ www/sony_o2o/views/index.py
@@ -7,8 +7,3 @@ BP = Blueprint('index', __name__)
 @BP.route('/')
 def frontend():
 	    return render_template("index.html")
-
-
-@BP.route('index')
-def index():
-		return render_template("index.html")
