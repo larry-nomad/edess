8a7df5bc0cc4369ad0c4b04a70c2aa5e848a4390
zsjforcn <zhangsanji@gmail.com>
2013-11-19 17:47:07 +0800

    结构调整

--- www/sony_o2o/static/js/tr.js
+++ www/sony_o2o/static/js/tr.js
@@ -8,18 +8,3 @@ var TemplateRender = function(template, data) {
     }
     return template;
 };
-
-$( document ).on( "pageinit", "#page-list", function() {
-    $( document ).on( "swipeleft swiperight", "#page-list", function( e ) {
-        // We check if there is no open panel on the page because otherwise
-        // a swipe to close the left panel would also open the right panel (and v.v.).
-        // We do this by checking the data that the framework stores on the page element (panel: open).
-        if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
-            if ( e.type === "swipeleft"  ) {
-                $( "#right-panel" ).panel( "open" );
-            } else if ( e.type === "swiperight" ) {
-                $( "#left-panel" ).panel( "open" );
-            }
-        }
-    });
-});
