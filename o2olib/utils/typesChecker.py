ec01ccffb5b1df9e6d76e0bd927428925f0e6a94
Liufang Deng <liufang.deng@qunar.com>
2013-12-04 19:22:52 +0800

    完善异常打印

--- o2olib/utils/typesChecker.py
+++ o2olib/utils/typesChecker.py
@@ -9,23 +9,41 @@ from flask.ext.restful import types
 from o2olib import logger
 from datetime import datetime
 import re
-from o2olib.QException import QException
 import six
+from functools import wraps
 
+"""
+reqparse中没有将异常打印出来，不便跟踪问题，只有在这里打印异常了
+"""
+def do_check_log(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        try:
+            rs = f(*args, **kwargs)
+        except TypeError,te:
+            raise te
+        except Exception,e:
+            logger.exception(e)
+            raise e
+        return rs
+    return decorated
+
+@do_check_log
 def check_email(val,name):
+    val = get_val(val)
     if not val:
-        return None    
-    'check email'
+        return val
     re_email = re.compile(r'^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$', re.IGNORECASE)
     re_email_res = re_email.match(val)
     if not re_email_res:
         raise ValueError(u"邮件格式 ’{}’ 不正确".format(val))
     return val
 
+@do_check_log
 def check_date(val,name):
+    val = get_val(val)
     if not val:
-        return None 
-    val = val.strip(" ")
+        return val
     date = None
     try: 
         date = datetime.strptime(val, u"%Y年%m月%d日")
@@ -38,19 +56,31 @@ def check_date(val,name):
         raise ValueError(u"Year must be >= 1900")
     return date  
 
+@do_check_log
 def check_int(val,name):
-    if val == None:
-        return None
-    val = val.strip(" ")    
+    val = get_val(val)
+    if not val:
+        return val
     return int(val)
 
+@do_check_log
 def check_str(val,name):
-    if val == None:
-        return None    
-    val = val.strip(" ")
+    val = get_val(val)
+    if not val:
+        return val
     return six.text_type(val)
 
+"""
+check_str方法里异常会被打印两遍
+"""
+@do_check_log
 def check_str_required(val,name):
-    if not val or not val.strip(" "):
+    val = get_val(val)
+    if not val:
         raise ValueError(u"val required")
     return check_str(val,name)
+
+def get_val(val):
+    if val == None:
+        return None
+    return val.strip(" ")
