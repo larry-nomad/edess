be6e1309209cd127edbee51448f097b3d4266edc
zsjforcn <zhangsanji@gmail.com>
2013-12-07 15:33:37 +0800

    分享功能，完成。

--- www/sony_o2o/static/js/page.js
+++ www/sony_o2o/static/js/page.js
@@ -1,5 +1,9 @@
 var CONST = {
-    stars: ['☆', '★']
+    stars: ['☆', '★'],
+    btn_weibo: '<wb:share-button data-role="none" type="button" addition="number" picture_search="false" pic="${pic}" default_text="${text}" language="zh_cn"></wb:share-button>',
+    js_weibo: '<script class="J-js-weibo" src="http://tjs.sjs.sinajs.cn/open/api/js/wb.js" charset="utf-8"></script>',
+    btn_qzone: '<a version="1.0" class="qzOpenerDiv" href="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?${query}" target="_blank">分享</a>',
+    js_qzone: '<script class="J-js-qzone" src="http://qzonestyle.gtimg.cn/qzone/app/qzlike/qzopensl.js#jsdate=20111201" charset="utf-8"></script>'
 };
 /**
  * 模版解析
@@ -304,6 +308,41 @@ $(document).on('pagebeforeshow', '#detail', function(e) {
         $('#J_detail_brief').html(data.brief);
         $('#J_detail_like').html(data.likes_count+'喜欢').button('refresh');
         $('#J_detail_buy_link').attr('href', data.jd_link || data.tmall_link || '#');
+
+        /**
+         * weibo按钮
+         */
+        $('.J-js-weibo').remove();
+        $('#J_detail_share_weibo').html(renderTemplate(CONST.btn_weibo, {
+            text: data.name,
+            pic: encodeURIComponent('http://touch.xperia.qunar.com/static/product_img/'+data.id+'/a.jpg')
+        }));
+        $(CONST.js_weibo).appendTo(document.body);
+
+        /**
+         * qzone按钮
+         */
+        $('.J-js-qzone').remove();
+        var p = {
+            url: location.href,
+            showcount: '1',/*是否显示分享总数,显示：'1'，不显示：'0' */
+            desc: data.name,/*默认分享理由(可选)*/
+            summary: data.brief,/*分享摘要(可选)*/
+            title: data.name,/*分享标题(可选)*/
+            site: '去哪儿网',/*分享来源 如：腾讯网(可选)*/
+            pics: 'http://touch.xperia.qunar.com/static/product_img/'+data.id+'/a.jpg', /*分享图片的路径(可选)*/
+            style: '102',
+            width: 145,
+            height: 30
+        };
+        var s = [];
+        for(var i in p){
+            s.push(i + '=' + encodeURIComponent(p[i]||''));
+        }
+        $('#J_detail_share_qzone').html(renderTemplate(CONST.btn_qzone, {
+            query: s.join('&')
+        }));
+        $(CONST.js_qzone).appendTo(document.body);
     });
     $.get(comment_url, function(res, status, xhr) {
         if(status !== 'success') {
