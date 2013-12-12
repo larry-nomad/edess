ead35a0282541834af7a7fbbb0aff987dd112398
陆研 <yan.lu@qunar.com>
2013-12-12 17:44:22 +0800

    fix weixin checkin

--- www/sony_o2o/views/weixin.py
+++ www/sony_o2o/views/weixin.py
@@ -7,21 +7,27 @@ BP = Blueprint('weixin', __name__)
 
 @BP.route('weixin')
 def weixin():
-    signature = request.args.get("signature")
-    timestamp = request.args.get("timestamp")
-    nonce = request.args.get("nonce")
-    echostr = request.args.get("echostr")
-    token = '9d3e73a365e1c8c5479ac0d1a341f15d'
-    tmp_arrary = [
-        token,
-        timestamp,
-        nonce
-    ]
-    tmp_arrary.sort()
-    tmp_string = ','.join(tmp_arrary)
-    tmp_string = hashlib.sha1(tmp_string).hexdigest()
+    if 'signature' in request.args and \
+            'timestamp' in request.args and \
+            'nonce' in request.args and \
+            'echostr' in request.args:
+        signature = request.args.get("signature")
+        timestamp = request.args.get("timestamp")
+        nonce = request.args.get("nonce")
+        echostr = request.args.get("echostr")
+        token = '9d3e73a365e1c8c5479ac0d1a341f15d'
+        tmp_arrary = [
+            token,
+            timestamp,
+            nonce
+        ]
+        tmp_arrary.sort()
+        tmp_string = ''.join(tmp_arrary)
+        tmp_string = hashlib.sha1(tmp_string).hexdigest()
 
-    if tmp_string == signature:
-        return echostr
+        if tmp_string == signature:
+            return echostr
+        else:
+            return tmp_string
     else:
-        return ''
+        return 'bad parameters'
